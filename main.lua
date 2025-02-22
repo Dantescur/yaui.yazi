3988813a6b671eed4fb32e39090a10d995b0e6b7
Cesar Daniel <cesardaniel.9611@gmail.com>
2025-02-22 13:24:23 -0500

    impr ui

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 --- @since 25.2.7
 
+local tx_ui, rx_ui = ya.chan("mpsc")
+local tx_pack, rx_pack = ya.chan("mpsc")
+
 local toggle_ui = ya.sync(function(self)
 	if self.children then
 		Modal:children_remove(self.children)
@@ -10,9 +13,57 @@ local toggle_ui = ya.sync(function(self)
 	ya.render()
 end)
 
+local function execute_pack_command(cmd)
+	local result = ya.popen("ya pack " .. cmd):read("*a")
+	return result
+end
+
+local function producer()
+	while true do
+		local action = rx_ui:recv()
+		if action == "quit" then
+			toggle_ui()
+			break
+		end
+	end
+end
+
+local function pack_consumer()
+	while true do
+		local command = rx_pack:recv()
+		if command then
+			local output = execute_pack_command(command)
+			print(output) -- Replace with UI feedback later
+		end
+	end
+end
+
 local M = {
 	keys = {
-		{ on = "q", run = "quit" },
+		{
+			on = "q",
+			run = function()
+				tx_ui:send("quit")
+			end,
+		}, -- Quit UI
+		{
+			on = "i",
+			run = function()
+				tx_pack:send("install <package>")
+			end,
+		}, -- Install package
+		{
+			on = "r",
+			run = function()
+				tx_pack:send("remove <package>")
+			end,
+		}, -- Remove package
+		{
+			on = "l",
+			run = function()
+				tx_pack:send("list")
+			end,
+		}, -- List packages
 	},
 }
 
@@ -29,20 +80,18 @@ function M:layout(area)
 			ui.Constraint.Percentage(10),
 		})
 		:split(area)
-
 	self._area = chunks[2] -- Set the UI area
 end
 
 function M:entry()
 	toggle_ui()
-	ya.render()
+	ya.join(producer, pack_consumer)
 end
 
 function M:redraw()
 	local rows = {
-		ui.Row({ "Column 1", "Column 2", "Column 3" }):style(ui.Style():bold()), -- Header
-		ui.Row({ "Row 1", "Data 1", "Data 2" }),
-		ui.Row({ "Row 2", "Data 3", "Data 4" }),
+		ui.Row({ "Package", "Version", "Status" }):style(ui.Style():bold()), -- Header
+		ui.Row({ "example", "1.0.0", "installed" }), -- Placeholder data
 	}
 
 	return {
@@ -51,11 +100,11 @@ function M:redraw()
 			:area(self._area)
 			:type(ui.Border.ROUNDED)
 			:style(ui.Style():fg("blue"))
-			:title(ui.Line("Test UI"):align(ui.Line.CENTER)),
+			:title(ui.Line("YA Pack Manager"):align(ui.Line.CENTER)),
 		ui.Table(rows):area(self._area:pad(ui.Pad(1, 2, 1, 2))):widths({
-			ui.Constraint.Percentage(30),
-			ui.Constraint.Percentage(30),
 			ui.Constraint.Percentage(40),
+			ui.Constraint.Percentage(30),
+			ui.Constraint.Percentage(30),
 		}),
 	}
 end
